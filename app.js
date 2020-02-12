35fcecd2e466be3e32211acc3c21d74a4e7ba377
Philip Iparaguirre <philip.iparaguirre@gmail.com>
2020-02-11 22:08:48 -0800

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password:"(Don't want to give out my password because I use it for everything. oops)",
+    password:"",
     database:"worker_db"
 });
 
